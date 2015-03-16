537feb508b83332d47beff89aeedf8e2e470d015
Greg Tatum <tatum.creative@gmail.com>
2015-03-16 10:55:15 -0500

    removed bezier curves

--- source/bundle.js
+++ source/bundle.js
@@ -325,179 +325,6 @@ Vector2.prototype = {
 module.exports = Vector2;
 },{}],2:[function(require,module,exports){
 var Vector2 = require('../dependancies/Vector2')
-var hslToFillStyle = require('./utils/hsl-to-fillstyle')
-
-var BezierCurveFromLine = function(line, smoothness) {
-	
-	//Control point naming assumes left to right for semantic meaning only
-	//Bezier curves can be any direction
-	
-	this.line = undefined
-	this.cpLeft = undefined
-	this.cpRight = undefined
-	this.smoothnes = undefined
-	this.distance = undefined
-	
-	if(_.isArray(line) && _.isNumber(smoothness)) {
-		
-		this.generate(line, smoothness)
-	}
-}
-
-BezierCurveFromLine.prototype = {
-	
-	generate : function( line, smoothness ) {
-		
-		var i, il,
-			p1, p2, p3,
-			distance,
-			totalDistance = 0,
-			distances = [],
-			theta,
-			cpLeft = [],
-			cpRight = []
-		
-		//Generate distances
-		for(i=1, il=line.length; i < il; i++) {
-			distance = Math.sqrt(
-				Math.pow( line[i-1].x - line[i].x, 2) +
-				Math.pow( line[i-1].y - line[i].y, 2)
-			)
-			distances.push( distance )
-			this.distance += distance
-		}
-		
-		//Add a beginning control point
-		cpLeft.push( new Vector2().copy(line[0]) )
-		cpRight.push( new Vector2().copy(line[0]) )
-		
-		//Generate control points
-		for(i=1, il=line.length - 1; i < il; i++) {
-			
-			p1 = line[i-1]
-			p2 = line[i]
-			p3 = line[i+1]
-			
-			d1 = distances[i-1]
-			d2 = distances[i]
-			
-			theta = Math.atan2(
-				p3.y - p1.y,
-				p3.x - p1.x
-			)
-			
-			cpLeft.push( new Vector2(
-				p2.x + ( d1 * smoothness ) * Math.cos( theta + Math.PI ),
-				p2.y + ( d1 * smoothness ) * Math.sin( theta + Math.PI )
-			))
-			
-			cpRight.push( new Vector2(
-				p2.x + ( d2 * smoothness ) * Math.cos( theta ),
-				p2.y + ( d2 * smoothness ) * Math.sin( theta )
-			))
-		}
-		
-		//Add an ending control point
-		cpLeft.push( new Vector2().copy(line[ line.length - 1 ]) )
-		cpRight.push( new Vector2().copy(line[ line.length - 1 ]) )
-		
-		this.line = line
-		this.cpLeft = cpLeft
-		this.cpRight = cpRight
-		this.smoothnes = smoothness
-		this.distance = totalDistance
-	},
-	
-	regenerate : function() {
-		this.generateBezier( this.line, this.smoothness )
-	},
-	
-	createBezierPath : function( ctx ) {
-
-		var i, il = this.line.length
-		
-		ctx.moveTo(line[0].x, line[0].y)
-	
-		for(i=1; i < il; i++) {
-			ctx.bezierCurveTo(
-				this.cpRight[i-1].x, this.cpRight[i-1].y,
-				this.cpLeft[i].x, this.cpLeft[i].y,
-				this.line[i].x, this.line[i].y
-			)
-		}
-	},
-	
-	drawCurve : function( ctx ) {
-		
-		ctx.lineWidth = 3
-		ctx.strokeStyle = hslToFillStyle(0, 50, 50, 0.5)
-		ctx.beginPath()
-		ctx.lineCap = "round"
-		
-		this.createBezierPath( ctx )
-		
-		ctx.stroke()
-		ctx.closePath()
-		
-	},
-	
-	drawControlPoints : function( ctx ) {
-		
-		ctx.lineCap = "round"
-		
-		for(var i=0; i < this.cpLeft.length; i++) {
-
-			ctx.lineWidth = 1
-			
-			ctx.beginPath()
-			ctx.moveTo( this.cpLeft[i].x, this.cpLeft[i].y )
-			ctx.lineTo( this.cpRight[i].x, this.cpRight[i].y )
-			ctx.strokeStyle = hslToFillStyle(135, 100, 25, 0.4)
-			ctx.stroke()
-			ctx.closePath()
-
-			ctx.lineWidth = 50
-			
-			
-			ctx.beginPath()
-			ctx.arc( this.cpLeft[i].x, this.cpLeft[i].y, 5, 0, 2 * Math.PI )
-			ctx.fillStyle = hslToFillStyle(90, 50, 50, 0.3)
-			ctx.fill()
-			ctx.closePath()
-			
-			ctx.beginPath()
-			ctx.arc( this.cpRight[i].x, this.cpRight[i].y, 5, 0, 2 * Math.PI )
-			ctx.fillStyle = hslToFillStyle(180, 50, 50, 0.3)
-			ctx.fill()
-			ctx.closePath()
-			
-			
-		}
-	},
-	
-	drawLineSegments : function( ctx ) {
-		var line = this.line
-		
-		ctx.lineWidth = 1
-		ctx.beginPath()
-		ctx.strokeStyle = hslToFillStyle(0, 0, 0, 0.3)
-		for(i=1; i < line.length; i++) {
-			prev = line[i-1]
-			curr = line[i]
-			
-			ctx.moveTo(prev.x,prev.y)
-			ctx.lineTo(curr.x,curr.y)
-		}
-		ctx.stroke()
-		ctx.closePath()
-	}
-}
-
-module.exports = BezierCurveFromLine
-
-},{"../dependancies/Vector2":1,"./utils/hsl-to-fillstyle":7}],3:[function(require,module,exports){
-var Vector2 = require('../dependancies/Vector2')
-  , BezierCurveFromLine = require('./bezier-curve')
   , hslToFillStyle = require('./utils/hsl-to-fillstyle')
 
 var DrawCurve = function( smoothness, canvas, context, callback ) {
@@ -635,20 +462,15 @@ DrawCurve.prototype = {
 		
 		line = this.smoothLine()
 		
-		_.each(line, function toUnitVector( vec2 ) {
-			vec2.x /= canvas.width
-			vec2.y /= canvas.height
+		var unitLine = _.map(line, function toUnitVector( vec2 ) {
+			return [
+				vec2.x / canvas.width,
+				vec2.y / canvas.height
+			]
 		})
 		
-		curve = new BezierCurveFromLine( line, 0.3 )
-		
-		if( this.doDrawCurve ) {
-			this.drawCurve( curve )
-		}
-		
-		
 		if(typeof this.callback === 'function') {
-			this.callback( curve )
+			this.callback( unitLine )
 		}
 		
 	},
@@ -735,14 +557,6 @@ DrawCurve.prototype = {
 		
 	},
 	
-	drawCurve : function( curve ) {
-		var ctx = this.context
-		
-		curve.drawLineSegments( ctx )
-		curve.drawCurve( ctx )
-		curve.drawControlPoints( ctx )
-	},
-	
 	drawMouseMove : function( prev, curr ) {
 		
 		var ctx = this.context
@@ -759,7 +573,7 @@ DrawCurve.prototype = {
 }
 
 module.exports = DrawCurve
-},{"../dependancies/Vector2":1,"./bezier-curve":2,"./utils/hsl-to-fillstyle":7}],4:[function(require,module,exports){
+},{"../dependancies/Vector2":1,"./utils/hsl-to-fillstyle":6}],3:[function(require,module,exports){
 var Scene = require('./scene')
 
 window.Generator = (function() {
@@ -824,7 +638,7 @@ window.Generator = (function() {
     }
 
 })();
-},{"./scene":6}],5:[function(require,module,exports){
+},{"./scene":5}],4:[function(require,module,exports){
 var Vector2 = require('../dependancies/Vector2')
 
 var LineNode = function() {
@@ -848,7 +662,7 @@ LineNode.prototype = {
 }
 
 module.exports = LineNode
-},{"../dependancies/Vector2":1}],6:[function(require,module,exports){
+},{"../dependancies/Vector2":1}],5:[function(require,module,exports){
 var DrawCurve = require('./draw-curve')
   , LineNode = require('./line-node')
   , hslToFillStyle = require('./utils/hsl-to-fillstyle')
@@ -930,11 +744,10 @@ Scene.prototype = {
 		}
 	},
 	
-	onNewCurve : function( curve ) {
+	onNewCurve : function( line ) {
 		var i, lineNode
 		
-		this.settings.unitCurve = curve
-		this.unitCurve = curve
+		this.settings.unitLine = line
 		
 		this.draw()
 	},
@@ -943,13 +756,13 @@ Scene.prototype = {
 		
 		this.reset()
 				
-		if( this.unitCurve ) {
+		if( this.settings.unitLine ) {
 			
-			var lines = _.map( this.unitCurve.line, function( line ) {
+			var lines = _.map( this.settings.unitLine, function( line ) {
 			
 				return new Vector2(
-					line.x * this.canvas.width,
-					line.y * this.canvas.height
+					line[0] * this.canvas.width,
+					line[1] * this.canvas.height
 				)
 			
 			}.bind(this))
@@ -1011,7 +824,7 @@ Scene.prototype = {
 }
 
 module.exports = Scene
-},{"../dependancies/Vector2":1,"./draw-curve":3,"./line-node":5,"./utils/hsl-to-fillstyle":7,"./utils/rgb-to-fillstyle":8}],7:[function(require,module,exports){
+},{"../dependancies/Vector2":1,"./draw-curve":2,"./line-node":4,"./utils/hsl-to-fillstyle":6,"./utils/rgb-to-fillstyle":7}],6:[function(require,module,exports){
 var hslToFillStyle = function(h, s, l, a) {
 	if(a === undefined) {
 		return ["hsl(",h,",",s,"%,",l,"%)"].join('')
@@ -1021,7 +834,7 @@ var hslToFillStyle = function(h, s, l, a) {
 }
 
 module.exports = hslToFillStyle
-},{}],8:[function(require,module,exports){
+},{}],7:[function(require,module,exports){
 var rgbToFillStyle = function(r, g, b, a) {
 	if(a === undefined) {
 		return ["rgb(",r,",",g,",",b,")"].join('')
@@ -1031,4 +844,4 @@ var rgbToFillStyle = function(r, g, b, a) {
 }
 
 module.exports = rgbToFillStyle
-},{}]},{},[4]);
+},{}]},{},[3]);
